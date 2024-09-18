66d235b615d8381d4bc6bc6f961e321f6d4b11b9
jayakrishnancn <jayakrishnancn@gmail.com>
2024-09-18 22:54:19 +0530

    persistentLocalCache

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,6 +17,9 @@ let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-export const firestoreDb = getFirestore(app);
+export const firestoreDb = initializeFirestore(app, {
+  localCache: 
+    persistentLocalCache(/*settings*/{tabManager: persistentMultipleTabManager()})
+});
 
 export default app;
