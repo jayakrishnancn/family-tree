2e78f590637d49c28cd28ce347906d789e5dc936
jayakrishnancn <jayakrishnancn@gmail.com>
2024-09-18 13:24:22 +0530

    login fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,15 +9,17 @@ const Login = () => {
   const [loading, setLoading] = useState(false);
   const handleLogin = async () => {
     setLoading(true);
-    try {
-      signInWithPopup(auth, provider).then((res) => {
+    signInWithPopup(auth, provider)
+      .then((res) => {
         console.log("User signed in", res);
         router.push("/");
+      })
+      .catch((error) => {
+        console.error("Error signing in: ", error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
-    } catch (error) {
-      console.error("Error signing in: ", error);
-      setLoading(false);
-    }
   };
 
   return (
